2ad9b0e7b8fecbb9778947261c1e23535d146047
yunseongjae <71856791+yunseongjae@users.noreply.github.com>
2024-09-25 13:40:30 +0900

    Update web_code_executor.py

--- web_code_executor.py
+++ web_code_executor.py
@@ -2,14 +2,16 @@ from typing import Annotated
 from typing_extensions import TypedDict
 from langgraph.graph.message import add_messages
 from langchain_core.messages import SystemMessage, HumanMessage
-
-class State(TypedDict):
-    messages: Annotated[list, add_messages]
-
 from langchain_community.tools.tavily_search import TavilySearchResults
 from langgraph.prebuilt import ToolNode, tools_condition
 from langchain_experimental.utilities import PythonREPL
 from langchain_core.tools import tool
+from langchain_openai import ChatOpenAI
+from langgraph.graph import StateGraph, START, END
+from langgraph.checkpoint.memory import MemorySaver
+
+class State(TypedDict):
+    messages: Annotated[list, add_messages]
 
 web_search = TavilySearchResults(max_results=2)
 repl = PythonREPL()
@@ -19,7 +21,8 @@ def python_repl(
     code: Annotated[str, "The python code to execute to generate your chart."],
 ):
     """Use this to execute python code. If you want to see the output of a value,
-    you should print it out with `print(...)`. This is visible to the user."""
+    you should print it out with `print(...)`. chart labels should be written in English.
+    This is visible to the user."""
     try:
         result = repl.run(code)
     except BaseException as e:
@@ -31,24 +34,43 @@ def python_repl(
 tools=[web_search, python_repl]
 tool_node = ToolNode(tools)
 
-from langchain_openai import ChatOpenAI
-
 llm = ChatOpenAI(model="gpt-4o-mini")
 llm_with_tools = llm.bind_tools(tools)
 
-def chatbot(state: State):
+def agent(state: State):
     result = llm_with_tools.invoke(state["messages"])
     return {"messages": [result]}
 
-from langgraph.graph import StateGraph
+def should_continue(state):
+    messages = state["messages"]
+    last_message = messages[-1]
+    # If there is no function call, then we finish
+    if not last_message.tool_calls:
+        return "end"
+    # Otherwise if there is, we continue
+    else:
+        return "continue"
 
-graph_builder = StateGraph(State)
 
-graph_builder.add_node("chatbot", chatbot)
-graph_builder.add_node("tools", tool_node)
 
-graph_builder.add_edge("tools", "chatbot")
-graph_builder.add_conditional_edges("chatbot", tools_condition)
+workflow = StateGraph(State)
 
-graph_builder.set_entry_point("chatbot")
-graph = graph_builder.compile()
+workflow.add_node("agent", agent)
+workflow.add_node("tool", tool_node)
+
+workflow.add_edge(START, "agent")
+workflow.add_conditional_edges(
+    "agent",
+    should_continue,
+    {
+        "continue": "tool",
+        # Otherwise we finish.
+        "end": END,
+    },
+)
+
+workflow.add_edge("tool", "agent")
+
+memory = MemorySaver()
+
+graph = workflow.compile(checkpointer=memory, interrupt_before=["tool"])
